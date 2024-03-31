cec0c5028c2f8b9a2c728dbadcc4a36613d904bb
pquoc693 <quoc.pv1999@aiaracorp.com>
2024-03-31 22:10:21 +0700

    product list page

--- client/src/apiRequests/product.ts
+++ client/src/apiRequests/product.ts
@@ -1,18 +1,19 @@
-import http from '@/lib/http'
+import http from "@/lib/http";
 import {
   CreateProductBodyType,
+  ProductListResType,
   ProductResType
-} from '@/schemaValidations/product.schema'
+} from "@/schemaValidations/product.schema";
 
 const productApiRequest = {
-  get: () => http.get('/products'),
+  getList: () => http.get<ProductListResType>("/products"),
   create: (body: CreateProductBodyType) =>
-    http.post<ProductResType>('/products', body),
+    http.post<ProductResType>("/products", body),
   uploadImage: (body: FormData) =>
     http.post<{
-      message: string
-      data: string
-    }>('/media/upload', body)
-}
+      message: string;
+      data: string;
+    }>("/media/upload", body)
+};
 
-export default productApiRequest
+export default productApiRequest;
